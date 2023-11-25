2fa52bd298bfd768a7b98b95db5ac350c8f924fd
virons <vironssatana@gmail.com>
2023-11-25 10:46:18 +0300

    Update vot-cloudflare.user.js

--- dist/vot-cloudflare.user.js
+++ dist/vot-cloudflare.user.js
@@ -909,7 +909,7 @@ const sitesProxyTok = [
 /* harmony export */   i: () => (/* binding */ workerHost)
 /* harmony export */ });
 // CLOUDFLARE CONFIGURATION
-const workerHost = "vot-new.toil-dump.workers.dev";
+const workerHost = "shy-hill-a8fe.vironssatana.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 
 
